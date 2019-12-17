9f3062742f6658a1ca15ee061486100b758fb6ea
Carl Ljungberg <ljungberg125@gmail.com>
2019-12-17 15:37:45 +0100

    initial commit, working streaming

---
+++ src/main/java/se/complexjava/videostreamer/VideoStreamerApplication.java
@@ -0,0 +1,13 @@
+package se.complexjava.videostreamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoStreamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoStreamerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/se/complexjava/videostreamer/rest/StreamingController.java
@@ -0,0 +1,57 @@
+package se.complexjava.videostreamer.rest;
+
+
+import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.support.ResourceRegion;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+
+@RestController
+public class StreamingController {
+
+
+    @GetMapping("/videos/{name}")
+    public ResponseEntity<ResourceRegion> getVideo(@PathVariable(name = "name") String name, @RequestHeader HttpHeaders headers) throws Exception{
+
+        Path file = Paths.get("./videos/" + name);
+        UrlResource video = new UrlResource(file.toUri());
+
+        ResourceRegion newResource = resourceRegion(video, headers);
+
+        return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+                .contentType(MediaTypeFactory
+                        .getMediaType(video)
+                        .orElse(MediaType.APPLICATION_OCTET_STREAM))
+                .body(newResource);
+    }
+
+
+    private ResourceRegion resourceRegion(UrlResource video, HttpHeaders headers) throws Exception{
+
+        long contentLength = video.contentLength();
+        List<HttpRange> range = headers.getRange();
+
+        if(!range.isEmpty()){
+
+            long start = range.get(0).getRangeStart(contentLength);//make this less hardcoded
+            long end = range.get(0).getRangeEnd(contentLength);//    -//-
+            long rangeLength = Long.min(1 * 1024 * 1024, end - start + 1);//sets rangeLength to max 1 mb
+
+            return new ResourceRegion(video, start, rangeLength);
+
+        }else{
+
+            long rangeLength = Long.min(1 * 1024 * 1024, contentLength);
+
+            return new ResourceRegion(video, 0, rangeLength);
+        }
+    }
+}
